7662cd0b46fc2cfd0eb26e5a295f9ea724dc68d9
griffindj <griffindj@gmail.com>
2014-10-23 22:48:53 -0400

    Added a more obvious message for when you run the app, to know that it ran.

--- trunk/twitterAlt/src/main/java/edu/umw/cpsc/twitterAlt/App.java
+++ trunk/twitterAlt/src/main/java/edu/umw/cpsc/twitterAlt/App.java
@@ -4,10 +4,12 @@ package edu.umw.cpsc.twitterAlt;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("\n\n\n\n\n");
+		System.out.println("Hello World!");
+		System.out
+				.println("If you're seeing this then you have successfully executed Maven");
+		System.out.println("\n\n\n\n\n");
+	}
 }
