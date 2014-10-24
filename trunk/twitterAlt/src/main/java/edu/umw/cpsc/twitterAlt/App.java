a835c7d0481d9c0740e6a3f255c72cbfce471963
griffindj <griffindj@gmail.com>
2014-10-24 01:46:01 -0400

    Added a more helpful startup message, say you should be able to open the app in your browser.

--- trunk/twitterAlt/src/main/java/edu/umw/cpsc/twitterAlt/App.java
+++ trunk/twitterAlt/src/main/java/edu/umw/cpsc/twitterAlt/App.java
@@ -10,11 +10,13 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("\n\n\n\n\n");
 		System.out.println("Hello World!");
-		System.out
-				.println("If you're seeing this then you have successfully executed Maven");
+		System.out.println("If you're seeing this, then odds are you"
+				+ " can open http://localhost:8082 in your web browser"
+				+ " and see our homepage");
 		System.out.println("\n\n\n\n\n");
-		
-		//this starts the spark server to begin listening on port 8082;
+
+		// this starts the spark server to begin listening on port 8082;
 		HttpServer.start();
+
 	}
 }
